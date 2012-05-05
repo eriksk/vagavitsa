0730417360e01090bcb511e596772673ecf271fa
Th3dz <erikskoglund88@gmail.com>
2012-05-05 17:12:48 +0200

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vagavitsa::Application.config.secret_token = '060032bbdc6be1b2cbc314519c1643cfd93f4432387d641a5cc0df53d35125bf9cab55d6a2ce4377fcb9a07ed97db6b1a0320dd6dca0a38303f5c575268761e3'
